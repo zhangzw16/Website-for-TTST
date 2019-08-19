7da8a0cf5b77c75979652854170e4436b3d712ee
zhangzw16 <THUzhangzw@outlook.com>
2019-08-19 22:07:57 +0800

    change to material-ui table

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -166,7 +166,6 @@ export default function Dashboard() {
             component={route.component}
           />
         ))}
-  
         <Copyright />
       </main>
     </div>
--- src/dashboard/Peoples.js
+++ src/dashboard/Peoples.js
@@ -1,15 +1,11 @@
 /* eslint-disable no-script-url */
 
 import React from 'react';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Title from './Title';
-
+import MaterialTable from 'material-table';
+import tableIcons from '../components/tableIcons';
+import axios from 'axios';
+// import { Save, Delete } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
   seeMore: {
@@ -19,7 +15,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function Peoples() {
   const classes = useStyles();
-  const [people, setPeople] = React.useState(null);
+  const [people, setPeople] = React.useState([]);
   
   React.useEffect(() => {
     fetch(process.env.REACT_APP_SERVER_IP + "api/people")
@@ -29,36 +25,66 @@ export default function Peoples() {
 
   return (
     <React.Fragment>
-      <Title>科服人员</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>学号</TableCell>
-            <TableCell>姓名</TableCell>
-            <TableCell>院系</TableCell>
-            <TableCell>联系电话</TableCell>
-            <TableCell>电子邮件</TableCell>
-            <TableCell>住址</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(people || []).map(row => (
-            <TableRow key={row._id}>
-              <TableCell>{row.学号}</TableCell>
-              <TableCell>{row.姓名}</TableCell>
-              <TableCell>{row.院系}</TableCell>
-              <TableCell>{row.联系电话}</TableCell>
-              <TableCell>{row.电子邮件}</TableCell>
-              <TableCell>{row.住址}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <div className={classes.seeMore}>
-        <Link color="primary">
-          See more orders
-        </Link>
-      </div>
+      {/* <Title>科服人员</Title> */}
+      <MaterialTable
+        icons={tableIcons}
+        title="科服暑期批次人员名单"
+        columns={[
+          { title: '学号', field: '学号' },
+          { title: '姓名', field: '姓名' },
+          { title: "性别", field: "性别" },
+          { title: '院系', field: '院系' },
+          { title: '联系电话', field: '联系电话' },
+          { title: '电子邮件', field: '电子邮件' },
+          { title: '住址', field: '住址' },
+        ]}
+        data={people}
+        editable={{
+          onRowAdd: newData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                {
+                  const data = people;
+                  data.push(newData);
+                  axios.post(process.env.REACT_APP_SERVER_IP + 'api/people', newData).then((res) => {
+                    setPeople(data);
+                  }).catch((error) => {
+                    console.error(error);
+                  })
+                }
+                resolve()
+              }, 1000)
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                {
+                  const data = this.state.data;
+                  const index = data.indexOf(oldData);
+                  data[index] = newData;
+                  this.setState({ data }, () => resolve());
+                }
+                resolve()
+              }, 1000)
+            }),
+          onRowDelete: oldData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                {
+                  let data = this.state.data;
+                  const index = data.indexOf(oldData);
+                  data.splice(index, 1);
+                  this.setState({ data }, () => resolve());
+                }
+                resolve()
+              }, 1000)
+            }),
+        }}
+        options={{
+          actionsColumnIndex: -1,
+          pageSize: 10
+        }}
+      />
     </React.Fragment>
   );
 }
