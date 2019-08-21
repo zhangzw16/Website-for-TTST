0d7c599013b5af78e0e87960aca54dac67a52df3
zhangzw16 <THUzhangzw@outlook.com>
2019-08-21 16:57:49 +0800

    use card in workingHours

--- src/dashboard/WorkingHours.js
+++ src/dashboard/WorkingHours.js
@@ -6,6 +6,10 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Deposits from 'dashboard/Deposits';
+// material-kit-react
+import Card from "components/Card/Card.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -23,15 +27,23 @@ export default function WorkingHours() {
       <Container maxWidth="lg" className={classes.container}>
         <Grid container spacing={3}>
           {/* Chart */}
-          <Grid item xs={12} md={8} lg={9}>
-            <Paper className={fixedHeightPaper}>
-              <Typography variant="h5" component="h3">
+          <Grid item xs md={8} lg={9}>
+              {/* <Typography variant="h5" component="h3">
                 This is a sheet of paper.
               </Typography>
               <Typography component="p">
                 Paper can be used to build surface or other elements for your application.
-              </Typography>
-            </Paper>
+              </Typography> */}
+              <Card style={{ width: "20rem" }}>
+                <CardHeader color="warning">Featured</CardHeader>
+                <CardBody>
+                  <h4 className={classes.cardTitle}>Special title treatment</h4>
+                  <p>
+                    With supporting text below as a
+                    natural lead-in to additional content.
+                  </p>
+                </CardBody>
+              </Card>
           </Grid>
           {/* Recent Deposits */}
           <Grid item xs={12} md={4} lg={3}>
